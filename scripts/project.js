971e607093048fe0c373996e718f6cd5dbd18594
Dan Oswalt <do189705@gmail.com>
2016-05-08 13:58:34 -0700

    Added data in project.js

---
+++ scripts/project.js
@@ -0,0 +1,13 @@
+var rawData = [
+
+  {
+    title: 'List War',
+    description: 'Prioritize your list with a war',
+    details: 'Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum Ipsum',
+    publishedOn: '2015-06-06',
+    publishedBy: 'Dan Oswalt',
+    url: 'http://danoswalt.com/ListWar/index.html',
+    codeUrl: 'https://github.com/DanOswalt/danoswalt.com/tree/master/ListWar',
+  }
+
+];
