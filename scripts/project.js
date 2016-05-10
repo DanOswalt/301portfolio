7b99ca94d192abe383873d701baa4f0fc217ceb3
Dan Oswalt <do189705@gmail.com>
2016-05-09 19:43:21 -0700

    moved all js into one place

--- scripts/project.js
+++ scripts/project.js
@@ -1,3 +1,7 @@
+/*****
+* class Project
+***/
+
 function Project (opts) {
   this.title = opts.title;
   this.description = opts.description;
@@ -14,16 +18,49 @@ Project.prototype.toHtml = function() {
 
   console.log($newProject.html());
 
-  $newProject.find('.title').html(this.title);
+  $newProject.find('.project-link').attr('href', this.url).html(this.title);
   $newProject.find('.description').html(this.description);
   $newProject.find('.details').html(this.details);
   $newProject.find('time').attr('datetime', this.publishedOn);
   $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newProject.find('.author').html(this.publishedBy);
-  $newProject.find('.title a').attr('href', this.url);
   $newProject.find('.code-link').attr('href', this.codeUrl);
   $newProject.find('img').attr('src', this.screenshot);
   $newProject.removeClass('template');
 
   return $newProject;
 };
+
+/*****
+* class ProjectModule
+***/
+
+function ProjectModule(projectData) {
+  //receives array of project data
+  this.data = projectData;
+};
+
+ProjectModule.prototype.load = function() {
+
+  //sort the data array
+  this.data.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+
+  //create new projects from projectData and add to html
+  this.data.forEach( function(projectData) {
+    var newProject = new Project(projectData);
+    $('#projects-module').append(newProject.toHtml());
+  });
+
+  //hide the template
+  $('.template').hide();
+};
+
+/****
+ * Code to on page load
+ ****/
+
+$(function() {
+  new ProjectModule(projectData).load();
+});
